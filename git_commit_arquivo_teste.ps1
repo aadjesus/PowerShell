ed4afad418fbcbb1d33a26c712b72888b5c7686e
aadjesus <aadjesus@outlook.com>
2024-09-19 14:28:27 -0300

    Atualizando arquivos

--- git_commit_arquivo_teste.ps1
+++ git_commit_arquivo_teste.ps1
@@ -1,11 +1,13 @@
-﻿$diretorio   = 'c:\Users\Alessandro.Augusto\GitHub'
+﻿clear
+$diretorio   = 'c:\Users\Alessandro.Augusto\GitHub'
 $nomeArquivo = 'teste.txt'
 $data        = Get-Date
 
 Get-ChildItem -Path $diretorio -Filter $nomeArquivo -Recurse -ErrorAction SilentlyContinue -Force | ForEach-object {    
-	write-host "Alterando arquivo"
+	cd $_.Directory
+    write-host "Alterando arquivo"
     Add-Content -Path $_.FullName -Value $data   
-
+    
 	write-host "Incluindo arquivo"
 	git add .	
 
