790f6d01849726e1aad4e696d5a246d148e763fd
aadjesus <aadjesus@outlook.com>
2024-10-01 10:00:21 -0300

    Atualizando arquivos

--- Procura_site_na_porta_80.ps1
+++ Procura_site_na_porta_80.ps1
@@ -1,4 +1,17 @@
-﻿Clear-Host
+﻿
+#https://pt.stackoverflow.com/questions/139796/toda-conex%C3%A3o-https-e-http-sempre-est%C3%A1-atrelada-a-porta-443-e-80-respectivamente
+
+#Por que usar portas diferentes?
+#	Obscurecimento: Ao usar uma porta não padrão, você torna menos óbvio o serviço que está sendo executado, o que pode dificultar ataques automatizados.
+#	Múltiplos sites na mesma máquina: Se você precisa hospedar vários sites na mesma máquina, usar portas diferentes permite isolar cada site, aumentando a segurança.
+#	Evitar conflitos: Em ambientes complexos, pode ser necessário usar portas diferentes para evitar conflitos com outros serviços.
+#Quais os riscos?
+#	Desconhecimento do usuário: A maioria dos usuários está acostumada a digitar apenas o nome do domínio, sem especificar a porta. Isso pode levar a dificuldades de acesso.
+#	Configuração de firewalls: Você precisará configurar os firewalls para permitir o tráfego na porta escolhida, o que pode ser um processo mais complexo.
+#	Menor compatibilidade: Alguns dispositivos ou softwares podem ter dificuldades para se conectar a portas não padrão.
+
+
+Clear-Host
 $sitePorta80 = (Get-Website | Where-Object { 
     (Get-WebBinding -name $_.name | Where-Object {
         $_.bindingInformation -match ':80:'
---
+++ Testando_url.ps1
@@ -0,0 +1,15 @@
+﻿clear 
+
+#try {
+#    $response = Invoke-RestMethod -Uri "http://localhost:8086/#/auth11"
+#    $response
+#} catch {
+#    $StatusCode = $_.Exception.Response.StatusCode
+
+#    $StatusCode
+#}
+#curl -Uri "https://localhost:44347/Health" 
+
+#$x1 = (New-Object System.Net.WebClient).DownloadString("https://localhost:44347/Health")
+$x1 = (New-Object System.Net.WebClient).DownloadString("http://localhost:8086/#/auth11")
+$x1 
