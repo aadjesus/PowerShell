61217ba279ea2c0d2a1bb68fb5281e4c8d8521bd
aadjesus <aadjesus@outlook.com>
2024-11-14 10:00:10 -0300

    Atualizando arquivos

--- Procura_Valor_no_arquivo.ps1
+++ Procura_Valor_no_arquivo.ps1
@@ -1,7 +1,9 @@
 ﻿clear 
 
+$diretorio = 'c:\Praxio\Praxio.RhTaOn\'
 
 $arquivo = Get-ChildItem -Path 'c:\Praxio\Praxio.RhTaOn\' -Recurse -Filter appsettings.json -ErrorAction SilentlyContinue | Select-Object -First 1
+$arquivo = Get-ChildItem -Path $diretorio -Recurse -Filter $nomeArquivo -ErrorAction SilentlyContinue | Select-Object -First 1
 
 $achouElasticSearch = $false
 if ($arquivo -ne $null) {
