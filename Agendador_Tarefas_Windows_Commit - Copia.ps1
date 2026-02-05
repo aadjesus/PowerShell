c2878d7279e3e608456a8734c70f34d7fb9dd2f3
aadjesus <aadjesus@outlook.com>
2026-02-05 17:25:53 -0300

    Atualizando arquivos

---
+++ Agendador_Tarefas_Windows_Commit - Copia.ps1
@@ -0,0 +1,41 @@
+﻿$agora      = Get-Date
+$limiteHoje = Get-Date -Hour 20 -Minute 0 -Second 0
+$quantidade = Get-Random -Minimum 2 -Maximum 6
+
+
+$triggers   = @()
+for ($i = 1; $i -le $quantidade; $i++) {
+    $segundosRestantes = ($limiteHoje - $agora).TotalSeconds
+    
+    if ($segundosRestantes -gt 0) {
+        $maximo = [Math]::Floor($segundosRestantes) 
+        $randomOffset = Get-Random -Minimum 0 -Maximum $maximo
+        $horarioSorteado = $agora.AddSeconds($randomOffset)
+        
+        $inicio = (Get-Date).AddMinutes($i)
+        $fim    = (Get-Date).AddMinutes($i).AddSeconds(10)
+                
+        $trigger = New-ScheduledTaskTrigger -Once -At $inicio        
+        $trigger.EndBoundary = $fim.ToString("yyyy-MM-ddTHH:mm:ss")
+
+        $triggers += $trigger
+    }
+}
+
+if ($triggers.Count -gt 0) {
+	$action = New-ScheduledTaskAction `
+		-Execute "powershell.exe" `
+		-Argument "-WindowStyle Hidden -ExecutionPolicy Bypass -File d:\GitHub\PowerShell\git_commit_arquivo_teste.ps1"
+
+    $settings = New-ScheduledTaskSettingsSet `
+        -DeleteExpiredTaskAfter (New-TimeSpan -Seconds 0)
+    $guid = [guid]::NewGuid().Guid
+    $taskName = "ALE - Commit automatico - $guid"
+    Register-ScheduledTask `
+        -TaskName $taskName `
+        -Action $action `
+        -Trigger $triggers `
+        -Settings $settings `
+        -User "SYSTEM" `
+        -Force
+}
--- Agendador_Tarefas_Windows_Commit.ps1
+++ Agendador_Tarefas_Windows_Commit.ps1
@@ -1,41 +1,52 @@
-﻿$agora      = Get-Date
-$limiteHoje = Get-Date -Hour 20 -Minute 0 -Second 0
+﻿cls
 $quantidade = Get-Random -Minimum 2 -Maximum 6
+$hoje18hs   = Get-Date   -Hour 18 -Minute 0 -Second 0
+$atual      = Get-Date
+#$atual      = Get-Date -Date "2026-02-05 08:00 AM"
 
+$tempoDisponivel  = [Math]::Round(($hoje18hs - $atual).TotalMinutes, 0) 
+if ($tempoDisponivel -le 0) {
+    return
+}
 
-$triggers   = @()
+$intervaloMinutos = $tempoDisponivel / $quantidade
+$triggers = @()
+
 for ($i = 1; $i -le $quantidade; $i++) {
-    $segundosRestantes = ($limiteHoje - $agora).TotalSeconds
+
+    $maximo = [Math]::Floor($intervaloMinutos / 2) 
+    $variacaoRandomica = Get-Random -Minimum 0 -Maximum $maximo
     
-    if ($segundosRestantes -gt 0) {
-        $maximo = [Math]::Floor($segundosRestantes) 
-        $randomOffset = Get-Random -Minimum 0 -Maximum $maximo
-        $horarioSorteado = $agora.AddSeconds($randomOffset)
-        
-        $inicio = (Get-Date).AddMinutes($i)
-        $fim    = (Get-Date).AddMinutes($i).AddSeconds(10)
-                
-        $trigger = New-ScheduledTaskTrigger -Once -At $inicio        
-        $trigger.EndBoundary = $fim.ToString("yyyy-MM-ddTHH:mm:ss")
-
-        $triggers += $trigger
+    $inicio = $atual.AddMinutes(($intervaloMinutos * $i) - $variacaoRandomica)
+    
+    if ($inicio -gt $hoje18hs) { 
+        $inicio = $hoje18hs.AddMinutes(-1) 
     }
+    $fim = $inicio.AddMinutes(2)
+
+    $trigger = New-ScheduledTaskTrigger -Once -At $inicio
+    $trigger.EndBoundary = $fim.ToString("yyyy-MM-ddTHH:mm:ss")
+
+    $triggers += $trigger
+
+    Write-Host "Trigger $i agendada para: $($inicio.ToString('HH:mm:ss')) : $($fim.ToString('HH:mm:ss'))" -ForegroundColor Cyan
 }
 
-if ($triggers.Count -gt 0) {
-	$action = New-ScheduledTaskAction `
-		-Execute "powershell.exe" `
-		-Argument "-WindowStyle Hidden -ExecutionPolicy Bypass -File d:\GitHub\PowerShell\git_commit_arquivo_teste.ps1"
+$currentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+$guid        = [guid]::NewGuid().Guid
+$taskName    = "ALE - Commit automatico - $guid"
 
-    $settings = New-ScheduledTaskSettingsSet `
-        -DeleteExpiredTaskAfter (New-TimeSpan -Seconds 0)
-    $guid = [guid]::NewGuid().Guid
-    $taskName = "ALE - Commit automatico - $guid"
-    Register-ScheduledTask `
-        -TaskName $taskName `
-        -Action $action `
-        -Trigger $triggers `
-        -Settings $settings `
-        -User "SYSTEM" `
-        -Force
-}
+$action = New-ScheduledTaskAction `
+	-Execute "powershell.exe" `
+	-Argument "-WindowStyle Hidden -ExecutionPolicy Bypass -File d:\GitHub\PowerShell\git_commit_arquivo_teste.ps1"
+
+$settings = New-ScheduledTaskSettingsSet `
+    -DeleteExpiredTaskAfter (New-TimeSpan -Seconds 0)
+
+Register-ScheduledTask `
+    -TaskName $taskName `
+    -Action $action `
+    -Trigger $triggers `
+    -Settings $settings `
+    -User $currentUser `
+    -Force
