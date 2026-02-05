d75cf400fc793cf89002d47d76472baf6f9fe0b1
aadjesus <aadjesus@outlook.com>
2026-02-05 18:19:54 -0300

    ajustes

--- git_commit_arquivo_teste.ps1
+++ git_commit_arquivo_teste.ps1
@@ -1,26 +1,44 @@
-﻿clear
+﻿cls
+try {
+    $diretorio   = 'd:\GitHub\'
+    $nomeArquivo = 'teste.txt'
+    $data        = Get-Date
 
-$diretorio   = 'd:\GitHub\'
-$nomeArquivo = 'teste.txt'
-$data        = Get-Date
+    $arquivos = Get-ChildItem `
+        -Path $diretorio `
+        -Filter $nomeArquivo `
+        -Recurse -Depth 1 `
+        -ErrorAction SilentlyContinue `
+        -Force
 
-Get-ChildItem -Path $diretorio -Filter $nomeArquivo -Recurse -ErrorAction SilentlyContinue -Force | ForEach-object {    
-    Write-Host "$($_.Directory): " -ForegroundColor Green
+    $qtde = Get-Random `
+        -Minimum 2 `
+        -Maximum ($arquivos.Count + 1)
 
-    try {       
-        Add-Content -Path $_.FullName -Value "$($data) -- $(New-Guid)"  
-            
-        cd $_.Directory
-	    git add .	
-	    git commit -m "Atualizando arquivos"
-	    git push
-    } catch {
-        Write-Error $_.Exception.Message
-    } finally {
-        $guid = [guid]::NewGuid().Guid
-        $arquivoLog = "C:\DestinationFolder\$($_.Directory.BaseName)_$($guid).txt"
+    $pathBaseLog = "c:\#Log\"
+    New-Item -Path $pathBaseLog -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
 
-        New-Item -Path $arquivoLog -ItemType "File"
-        Add-Content -Path $arquivoLog -Value "$($data) -- $(New-Guid)"  
-    }
+    $arquivoLog = "$($pathBaseLog)\Commit_Automatico_$(Get-Date -Format 'yyyyMMdd_HHmmss').txt"
+
+    Start-Transcript -Path $arquivoLog -Append
+
+    $sorteados = $arquivos | Get-Random -Count $qtde    
+    foreach ($item in $sorteados) {   
+        Write-Host "Diretorio: $($item.Directory)"
+
+        try {       
+            Add-Content -Path $_.FullName -Value "$($data) -- $(New-Guid)"  
+                
+            cd $_.Directory
+	        git add .	
+	        git commit -m "Atualizando arquivos"
+	        git push
+        } catch {
+            Write-Error $_.Exception.Message
+        }
+    } 
+} catch {
+    Write-Error "Erro: $_"
+} finally {
+    Stop-Transcript
 }
