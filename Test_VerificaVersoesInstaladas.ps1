6e07391a76c094e5d3df9ce1ea1fe747e4808e22
aadjesus <aadjesus@outlook.com>
2026-02-09 11:28:40 -0300

    Atualizando arquivos

--- Test_VerificaVersoesInstaladas.ps1
+++ Test_VerificaVersoesInstaladas.ps1
@@ -9,17 +9,20 @@ $Env:RELEASE_ARTIFACTS_GlobusWeb_Fretamento_back_end_DEFINITIONNAME    = "Globus
 $Env:RELEASE_ARTIFACTS_GlobusWeb_Motor_back_end_DEFINITIONNAME         = "GlobusWeb.Motor.back-end"                               
 
 
-$Env:RELEASE_ARTIFACTS_GlobusWeb_Setup_BUILDNUMBER                  = "1"
-$Env:RELEASE_ARTIFACTS_GlobusWeb_Acidentes_Front_End_BUILDNUMBER    = "2"
-$Env:RELEASE_ARTIFACTS_GlobusWeb_Acidentes_Back_End_BUILDNUMBER     = "3"
+$Env:RELEASE_ARTIFACTS_GlobusWeb_Setup_BUILDNUMBER                  = "1ssssssss"
+$Env:RELEASE_ARTIFACTS_GlobusWeb_Acidentes_Front_End_BUILDNUMBER    = "2ssssss"
+$Env:RELEASE_ARTIFACTS_GlobusWeb_Acidentes_Back_End_BUILDNUMBER     = "1.504.0-alpha6"
 $Env:RELEASE_ARTIFACTS_GlobusWeb_Abastecimento_back_end_BUILDNUMBER = "20250708.21"
-$Env:RELEASE_ARTIFACTS_GlobusWeb_Estoque_Back_end_BUILDNUMBER       = "5"
-$Env:RELEASE_ARTIFACTS_GlobusWeb_Fretamento_back_end_BUILDNUMBER    = "6"
-$Env:RELEASE_ARTIFACTS_GLOBUSWEB_MOTOR_BACK_END_BUILDNUMBER         = "7"
+$Env:RELEASE_ARTIFACTS_GlobusWeb_Estoque_Back_end_BUILDNUMBER       = "5sssssss"
+$Env:RELEASE_ARTIFACTS_GlobusWeb_Fretamento_back_end_BUILDNUMBER    = "6ssssss"
+$Env:RELEASE_ARTIFACTS_GLOBUSWEB_MOTOR_BACK_END_BUILDNUMBER         = "7ssssss"
 
-
+$Env:PATH_BASE = "Servers"
+$Env:PORTA_PARENT_WEBSITE = "80"
 $Env:AGENT_WORKFOLDER = "c:\azagent\A1\_work\"
-C:\Praxio\GlobusWeb\GlobusWeb.Setup\Scripts\VerificaVersoesInstaladas.ps1
 
-#GlobusWeb.Abastecimento.back.end
-#1234567890123456789012345678901234567890
+$Env:IGNORAR_GlobusWeb_Acidentes_Back_End  = $null
+#$Env:IGNORAR_GlobusWeb_Acidentes_Front_End = $null
+#$Env:IGNORAR_GlobusWeb_Acidentes_Front_End = 0
+$Env:FORCAR_ATUALIZACAO_GlobusWeb_Acidentes_Back_End  = 1
+C:\Praxio\GlobusWeb\GlobusWeb.Setup\Scripts\VerificaVersoesInstaladas.ps1
