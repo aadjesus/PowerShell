9726fa37fe95082e4b983b51dad04edfec55f7f9
aadjesus <aadjesus@outlook.com>
2026-02-11 16:32:04 -0300

    Atualizando arquivos

--- Test_ValidarAplicativosInstaladosNoIIS.ps1
+++ Test_ValidarAplicativosInstaladosNoIIS.ps1
@@ -1,8 +1,9 @@
 ﻿cls
 
-$Env:PARENT_WEBSITE = "80"
-#$Env:PATH_BASE = "Servers"
+
 $Env:PATH_DESTINO = "c:\Servers\GlobusWeb\"
 
 C:\Praxio\GlobusWeb\GlobusWeb.Setup\Scripts\ValidarAplicativosInstaladosNoIIS.ps1
 #c:\azagent\A1\_work\_GlobusWeb\Scripts\ValidarAplicativosInstaladosNoIIS.ps1
+#  http://172.16.0.3/GlobusWeb/gateway/api/health/rest
+#http://localhost:80/GlobusWeb/gateway/Api/health/rest
