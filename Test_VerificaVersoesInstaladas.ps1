abebdd5822b656d7ae243f8a2086a886c32b5961
aadjesus <aadjesus@outlook.com>
2026-02-12 16:36:16 -0300

    Atualizando arquivos

--- Test_VerificaVersoesInstaladas.ps1
+++ Test_VerificaVersoesInstaladas.ps1
@@ -1,28 +1,33 @@
 ﻿cls
- 
-$Env:RELEASE_ARTIFACTS_GlobusWeb_Setup_DEFINITIONNAME                  = "GlobusWeb.Setup"                                                 
-$Env:RELEASE_ARTIFACTS_GlobusWeb_Acidentes_Front_End_DEFINITIONNAME    = "GlobusWeb.Acidentes.Front.End"                     
-$Env:RELEASE_ARTIFACTS_GlobusWeb_Acidentes_Back_End_DEFINITIONNAME     = "GlobusWeb.Acidentes.Back.End"                       
-$Env:RELEASE_ARTIFACTS_GlobusWeb_Abastecimento_back_end_DEFINITIONNAME = "GlobusWeb.Abastecimento.back.end"
-$Env:RELEASE_ARTIFACTS_GlobusWeb_Estoque_Back_end_DEFINITIONNAME       = "GlobusWeb.Estoque.Back-end"                           
-$Env:RELEASE_ARTIFACTS_GlobusWeb_Fretamento_back_end_DEFINITIONNAME    = "GlobusWeb.Fretamento.back-end"                     
-$Env:RELEASE_ARTIFACTS_GlobusWeb_Motor_back_end_DEFINITIONNAME         = "GlobusWeb.Motor.back-end"                               
 
+$Env:RELEASE_ARTIFACTS_GlobusWeb_Setup_DEFINITIONNAME                  = "GlobusWeb.Setup"                     
+$Env:RELEASE_ARTIFACTS_GlobusWeb_Portal_Front_End_DEFINITIONNAME       = "GlobusWeb.Portal.Front.End"          
+$Env:RELEASE_ARTIFACTS_GlobusWeb_Acidentes_Front_End_DEFINITIONNAME    = "GlobusWeb.Acidentes.Front.End"       
+$Env:RELEASE_ARTIFACTS_GlobusWeb_Acidentes_Back_End_DEFINITIONNAME     = "GlobusWeb.Acidentes.Back.End"        
+$Env:RELEASE_ARTIFACTS_GlobusWeb_Manutencao_Front_End_DEFINITIONNAME   = "GlobusWeb.Manutencao.Front.End"      
+$Env:RELEASE_ARTIFACTS_GlobusWeb_Manutencao_Back_End_DEFINITIONNAME    = "GlobusWeb.Manutencao.Back.End"       
+$Env:RELEASE_ARTIFACTS_GlobusWeb_Gateway_Front_End_DEFINITIONNAME      = "GlobusWeb.Gateway.Front.End"         
+$Env:RELEASE_ARTIFACTS_GlobusWeb_Gateway_Back_End_DEFINITIONNAME       = "GlobusWeb.Gateway.Back.End"          
 
-$Env:RELEASE_ARTIFACTS_GlobusWeb_Setup_BUILDNUMBER                  = "1ssssssss"
-$Env:RELEASE_ARTIFACTS_GlobusWeb_Acidentes_Front_End_BUILDNUMBER    = "2ssssss"
-$Env:RELEASE_ARTIFACTS_GlobusWeb_Acidentes_Back_End_BUILDNUMBER     = "1.504.0-alpha6"
-$Env:RELEASE_ARTIFACTS_GlobusWeb_Abastecimento_back_end_BUILDNUMBER = "20250708.21"
-$Env:RELEASE_ARTIFACTS_GlobusWeb_Estoque_Back_end_BUILDNUMBER       = "5sssssss"
-$Env:RELEASE_ARTIFACTS_GlobusWeb_Fretamento_back_end_BUILDNUMBER    = "6ssssss"
-$Env:RELEASE_ARTIFACTS_GLOBUSWEB_MOTOR_BACK_END_BUILDNUMBER         = "7ssssss"
+$Env:RELEASE_ARTIFACTS_GlobusWeb_Setup_BUILDNUMBER                =  "1.7.0-beta1"
+$Env:RELEASE_ARTIFACTS_GlobusWeb_Portal_Front_End_BUILDNUMBER     =  "1.4.0-alpha3"
+$Env:RELEASE_ARTIFACTS_GlobusWeb_Acidentes_Front_End_BUILDNUMBER  =  "20260211.2"
+$Env:RELEASE_ARTIFACTS_GlobusWeb_Acidentes_Back_End_BUILDNUMBER   =  "20260212.1"
+$Env:RELEASE_ARTIFACTS_GlobusWeb_Manutencao_Front_End_BUILDNUMBER =  "20260212.3"
+$Env:RELEASE_ARTIFACTS_GlobusWeb_Manutencao_Back_End_BUILDNUMBER  =  "20260212.4"
+$Env:RELEASE_ARTIFACTS_GlobusWeb_Gateway_Front_End_BUILDNUMBER    =  "20260211.1"
+$Env:RELEASE_ARTIFACTS_GlobusWeb_Gateway_Back_End_BUILDNUMBER     =  "20260212.21"
+
 
 $Env:PATH_BASE = "Servers"
 $Env:PORTA_PARENT_WEBSITE = "80"
 $Env:AGENT_WORKFOLDER = "c:\azagent\A1\_work\"
+$Env:FORCAR_ATUALIZACAO = 11
 
-$Env:IGNORAR_GlobusWeb_Acidentes_Back_End  = $null
+#$Env:IGNORAR_GlobusWeb_Acidentes_Back_End  = $null
 #$Env:IGNORAR_GlobusWeb_Acidentes_Front_End = $null
 #$Env:IGNORAR_GlobusWeb_Acidentes_Front_End = 0
-$Env:FORCAR_ATUALIZACAO_GlobusWeb_Acidentes_Back_End  = 1
+$Env:FORCAR_ATUALIZACAO_GlobusWeb_PORTAL_FRONT_END  = 11
+#$Env:IGNORAR_VALIDAR_SISTEMA_GLOBUSWEB_ACIDENTES_FRONT_END = 11
+
 C:\Praxio\GlobusWeb\GlobusWeb.Setup\Scripts\VerificaVersoesInstaladas.ps1
