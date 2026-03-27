4816a726775b4c43009a94f5b9d4eec6ad5f0e4f
aadjesus <aadjesus@outlook.com>
2026-03-27 13:04:13 -0300

    Atualizando arquivos

--- Test_ValidarBranch.ps1
+++ Test_ValidarBranch.ps1
@@ -5,11 +5,16 @@ $Env:BUILD_SOURCEBRANCHNAME     = "Develop"
 $Env:RELEASE_RELEASEDESCRIPTION = ""
 $Env:SYSTEM_ACCESSTOKEN         = [Environment]::GetEnvironmentVariable("XXXXXXXX", "User")
 
-
 $Env:SYSTEM_TEAMFOUNDATIONSERVERURI = "https://vsrm.dev.azure.com/praxio/"
 $Env:SYSTEM_TEAMPROJECTID           = "f7e1f0f0-eca6-49ed-89c4-b580797f7a63"
 $Env:RELEASE_RELEASEID              = 85259
 $Env:RELEASE_ENVIRONMENTID          = 117781
+
+$Env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI = "https://dev.azure.com/praxio/"
+$Env:SYSTEM_TEAMPROJECT                 = "f7e1f0f0-eca6-49ed-89c4-b580797f7a63"
+$Env:AGENT_DEPLOYMENTGROUPID            = 540
+$Env:AGENT_MACHINENAME                  = "AAUGUSTO"
+$Env:AGENT_NAME                         = "AAUGUSTO"
 
 C:\Praxio\GlobusWeb\GlobusWeb.Setup\Scripts\ValidarBranch.ps1
 
