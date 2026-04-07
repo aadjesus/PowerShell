5d911e57461d9a923fe42d9863484301a8164618
aadjesus <aadjesus@outlook.com>
2026-04-07 10:25:58 -0300

    Atualizando arquivos

--- Test_ValidarBranch.ps1
+++ Test_ValidarBranch.ps1
@@ -16,5 +16,8 @@ $Env:AGENT_DEPLOYMENTGROUPID            = 540
 $Env:AGENT_MACHINENAME                  = "AAUGUSTO"
 $Env:AGENT_NAME                         = "AAUGUSTO"
 
+#$Env:AGENT_MACHINENAME                  = "PRXGLBWEB"
+#$Env:AGENT_NAME                         = "PRXGLBWEB"
+
 C:\Praxio\GlobusWeb\GlobusWeb.Setup\Scripts\ValidarBranch.ps1
 
