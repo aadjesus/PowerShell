c88fcf823fb200debf2f2f6ce2aca67236b0f734
aadjesus <aadjesus@outlook.com>
2026-04-09 16:49:20 -0300

    Atualizando arquivos

---
+++ Alterar_dados_do_agent.ps1
@@ -0,0 +1,85 @@
+﻿$textoScript = @"
+try {
+    function Get-Servico {
+         Write-Host "Procurando serviço"
+
+        `$retorno = Get-CimInstance -ClassName Win32_Service -Filter "Name LIKE '%praxio%'" | Select-Object -First 1
+        if (-not `$retorno){
+            throw "Serviço `$nome não encontrado."
+        }
+        return `$retorno
+    }    
+
+    `$arquivoLog = "`$(AGENT.RELEASEDIRECTORY)\reinstalar_agent_`$(Get-Date -Format 'yyyyMMdd_HHmmss').txt"    
+    Start-Transcript -Path `$arquivoLog -Append
+
+    Write-Host "Iniciando processo de reinstalação..."
+
+    `$servico = Get-Servico
+    `$numero  = `$servico.PathName | Select-String 'A(\d+)' | % { [int]`$_.Matches.Groups[1].Value } 
+    `$pasta   = "C:\azagent\A`$(`$numero)"    
+    cd `$pasta
+
+    Write-Host "--------------------------------------------------------------------------"
+    Write-Host "Removendo agente antigo da pasta `$pasta..."
+
+    .\config.cmd ``
+          remove ``
+          --unattended ``
+          --auth PAT ``
+          --token `$(TOKEN)
+
+    Write-Host "--------------------------------------------------------------------------"
+    Write-Host "Configurando novo agente: `$agentName"
+
+    .\config.cmd ``
+          --unattended ``
+          --deploymentgroup ``
+          --deploymentgroupname "`$(deployment_group)" ``
+          --runasservice ``
+          --work _work ``
+          --url "`$(SYSTEM.TASKDEFINITIONSURI)" ``
+          --projectname "`$(SYSTEM.TEAMPROJECT)" ``
+          --addDeploymentGroupTags ``
+          --deploymentGroupTags "`$(TAGS)" ``
+          --auth PAT ``
+          --token `$(TOKEN)
+    
+    `$servico = Get-Servico
+    sc.exe description `$servico.name "`$(DESCRICAO) - `$(Get-Date -Format 'dd/MM/yyyy')"
+
+    Write-Host "Processo finalizado com sucesso."
+
+} catch {
+    Write-Error "Erro detectado: `$_"
+} finally {
+    Stop-Transcript
+}
+"@
+
+$pathScript  = "$(AGENT.RELEASEDIRECTORY)\reinstalar_agent.ps1"
+
+Write-Host "Criando arquivo: $($pathScript)"
+$textoScript | Out-File -FilePath $pathScript -Encoding utf8 -Force
+
+Write-Host "Criando tarefa para executar o script e exclui-lo apos execução "
+$action = New-ScheduledTaskAction `
+	-Execute "powershell.exe" `
+	-Argument "-ExecutionPolicy Bypass -Command `"& '$pathScript'; Remove-Item -Path '$pathScript' -Force -ErrorAction SilentlyContinue`""
+
+$inicio = (Get-Date).AddMinutes(1)
+$fim    = (Get-Date).AddMinutes(2)
+
+$trigger  = New-ScheduledTaskTrigger -Once -At $inicio
+$trigger.EndBoundary = $fim.ToString("yyyy-MM-ddTHH:mm:ss")
+
+$settings = New-ScheduledTaskSettingsSet `
+	-DeleteExpiredTaskAfter (New-TimeSpan -Seconds 0)
+
+Register-ScheduledTask `
+	-TaskName "Praxio-Agente" `
+	-Action $action `
+	-Trigger $trigger `
+	-Settings $settings `
+	-User "SYSTEM" `
+        -Force
---
+++ alterar_web_config_para_node_com_nvm.ps1
@@ -0,0 +1,23 @@
+﻿cls
+
+$sourceApi = "C:\temp\web.config-API\web.config"
+$sourceUi  = "C:\temp\web.config-UI\web.config"
+
+$rootPath = "C:\Servers\GlobusWeb"
+
+
+$configs = Get-ChildItem -Path $rootPath -Filter "web.config" -Recurse
+
+Write-Host "Iniciando processamento de arquivos..."
+
+foreach ($file in $configs) {
+    $parentPath = $file.DirectoryName
+    $origem = if ($parentPath -like "*\API" -or $parentPath -like "*\API\*") { $sourceApi } else { $sourceUi }
+
+
+    Write-Host "Copiando: $($origem) para: $($file.DirectoryName)"
+
+    Copy-Item -Path $origem -Destination $file.FullName -Force
+}
+
+Write-Host "`nProcesso concluído!" -ForegroundColor White -BackgroundColor DarkGreen
