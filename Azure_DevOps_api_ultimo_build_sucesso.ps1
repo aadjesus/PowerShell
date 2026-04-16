c0ed52fa1dde0a2a72b54ac9acb57efb1afb32c1
aadjesus <aadjesus@outlook.com>
2026-04-16 12:02:53 -0300

    Atualizando arquivos

--- Azure_DevOps_api_ultimo_build_sucesso.ps1
+++ Azure_DevOps_api_ultimo_build_sucesso.ps1
@@ -16,19 +16,21 @@ $response = Invoke-RestMethod -Uri $uri -Method Get -Headers $headers
 #                                @{Name='name'; Expression={$_.definition.name}},
 #                                @{Name='finishTime'; Expression={$_.finishTime}}
 
-$latestPerPipeline = $response.value | 
-    Where-Object { $_.definition.name -match 'GlobusWeb.*End' } |
-    Group-Object { $_.definition.id } | 
-    ForEach-Object {
-        $_.Group | Sort-Object finishTime -Descending | Select-Object -First 1
-    }
+#$latestPerPipeline = $response.value | 
+#    Where-Object { $_.definition.name -match 'GlobusWeb.*End' } |
+#    Group-Object { $_.definition.id } | 
+#    ForEach-Object {
+#        $_.Group | Sort-Object finishTime -Descending | Select-Object -First 1
+#    }
+#
+#$latestPerPipeline | Select-Object `
+#    @{n="BuildID"; e={$_.id}},
+#    @{n="PipelineID"; e={$_.definition.id}},
+#    @{n="PipelineName"; e={$_.definition.name}},
+#    sourceBranch,
+#    buildNumber,
+#    finishTime | Sort-Object { [DateTime]$_.finishTime } -Descending | Format-Table -AutoSize
+#
+#$latestPerPipeline.Count
 
-$latestPerPipeline | Select-Object `
-    @{n="BuildID"; e={$_.id}},
-    @{n="PipelineID"; e={$_.definition.id}},
-    @{n="PipelineName"; e={$_.definition.name}},
-    sourceBranch,
-    buildNumber,
-    finishTime | Sort-Object { [DateTime]$_.finishTime } -Descending | Format-Table -AutoSize
-
-$latestPerPipeline.Count
+$response.value | ConvertTo-Json 
