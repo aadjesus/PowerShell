fe9dcf7146f048e7f53cce2bbb732e1b3a5a8955
aadjesus <aadjesus@outlook.com>
2026-04-20 08:42:17 -0300

    Atualizando arquivos

--- Test_ValidarBranch.ps1
+++ Test_ValidarBranch.ps1
@@ -1,6 +1,6 @@
 ﻿cls
 
-$Env:RELEASE_ENVIRONMENTNAME    = "Produção"
+$Env:RELEASE_ENVIRONMENTNAME    = "QA"
 $Env:BUILD_SOURCEBRANCHNAME     = "Develop"
 $Env:RELEASE_RELEASEDESCRIPTION = ""
 $Env:SYSTEM_ACCESSTOKEN         = [Environment]::GetEnvironmentVariable("XXXXXXXX", "User")
@@ -10,7 +10,7 @@ $Env:SYSTEM_TEAMPROJECTID           = "f7e1f0f0-eca6-49ed-89c4-b580797f7a63"
 $Env:RELEASE_RELEASEID              = 85499
 $Env:RELEASE_ENVIRONMENTID          = 118288
 
-$Env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI = "https://dev.azure.com/praxio/"
+$Env:SYSTEM_TASKDEFINITIONSURI = "https://dev.azure.com/praxio/"
 $Env:SYSTEM_TEAMPROJECT                 = "f7e1f0f0-eca6-49ed-89c4-b580797f7a63"
 $Env:AGENT_DEPLOYMENTGROUPID            = 540
 $Env:AGENT_MACHINENAME                  = "AAUGUSTO"
--- Test_VerificaVersoesInstaladas.ps1
+++ Test_VerificaVersoesInstaladas.ps1
@@ -1,36 +1,13 @@
 ﻿cls
 
-$Env:RELEASE_ARTIFACTS_GlobusWeb_Setup_DEFINITIONNAME                  = "GlobusWeb.Setup"                     
-$Env:RELEASE_ARTIFACTS_GlobusWeb_Portal_Front_End_DEFINITIONNAME       = "GlobusWeb.Portal.Front.End"          
-$Env:RELEASE_ARTIFACTS_GlobusWeb_Acidentes_Front_End_DEFINITIONNAME    = "GlobusWeb.Acidentes.Front.End"       
-$Env:RELEASE_ARTIFACTS_GlobusWeb_Acidentes_Back_End_DEFINITIONNAME     = "GlobusWeb.Acidentes.Back.End"        
-$Env:RELEASE_ARTIFACTS_GlobusWeb_Manutencao_Front_End_DEFINITIONNAME   = "GlobusWeb.Manutencao.Front.End"      
-$Env:RELEASE_ARTIFACTS_GlobusWeb_Manutencao_Back_End_DEFINITIONNAME    = "GlobusWeb.Manutencao.Back.End"       
-$Env:RELEASE_ARTIFACTS_GlobusWeb_Gateway_Front_End_DEFINITIONNAME      = "GlobusWeb.Gateway.Front.End"         
-$Env:RELEASE_ARTIFACTS_GlobusWeb_Gateway_Back_End_DEFINITIONNAME       = "GlobusWeb.Gateway.Back.End"          
-$Env:RELEASE_ARTIFACTS_GLOBUSWEB_TRANSPORTERODOVIARIODERISCOS_BACK_END_DEFINITIONNAME   = "GlobusWeb.TRANSPORTERODOVIARIODERISCOS.Back.End"
-$Env:RELEASE_ARTIFACTS_GLOBUSWEB_DOCUMENTACAODEVEICULOSESEGUROS_BACK_END_DEFINITIONNAME = "GLOBUSWEB.DOCUMENTACAODEVEICULOSESEGUROS.BACK.END"
-
-$Env:RELEASE_ARTIFACTS_GlobusWeb_Setup_BUILDNUMBER                =  "1.7.0-beta1"
-$Env:RELEASE_ARTIFACTS_GlobusWeb_Portal_Front_End_BUILDNUMBER     =  "1.4.0-alpha3"
-$Env:RELEASE_ARTIFACTS_GlobusWeb_Acidentes_Front_End_BUILDNUMBER  =  "20260409.1"
-$Env:RELEASE_ARTIFACTS_GlobusWeb_Acidentes_Back_End_BUILDNUMBER   =  "20260212.1"
-$Env:RELEASE_ARTIFACTS_GlobusWeb_Manutencao_Front_End_BUILDNUMBER =  "20260212.3"
-$Env:RELEASE_ARTIFACTS_GlobusWeb_Manutencao_Back_End_BUILDNUMBER  =  "20260212.4"
-$Env:RELEASE_ARTIFACTS_GlobusWeb_Gateway_Front_End_BUILDNUMBER    =  "20260211.1"
-$Env:RELEASE_ARTIFACTS_GlobusWeb_Gateway_Back_End_BUILDNUMBER     =  "20260212.21"
-$Env:RELEASE_ARTIFACTS_GLOBUSWEB_TRANSPORTERODOVIARIODERISCOS_BACK_END_BUILDNUMBER   =  "20260212.21"
-$Env:RELEASE_ARTIFACTS_GLOBUSWEB_DOCUMENTACAODEVEICULOSESEGUROS_BACK_END_BUILDNUMBER =  "20260212.21"
-
 $Env:PATH_BASE            = "Servers"
 $Env:PORTA_PARENT_WEBSITE = "80"
 $Env:AGENT_WORKFOLDER     = "c:\azagent\A1\_work\"
 $Env:FORCAR_ATUALIZACAO   = 11
 
-#$Env:IGNORAR_GlobusWeb_Acidentes_Back_End  = $null
-#$Env:IGNORAR_GlobusWeb_Acidentes_Front_End = $null
-#$Env:IGNORAR_GlobusWeb_Acidentes_Front_End = 0
-$Env:FORCAR_ATUALIZACAO_GlobusWeb_PORTAL_FRONT_END  = 11
-#$Env:IGNORAR_VALIDAR_SISTEMA_GLOBUSWEB_ACIDENTES_FRONT_END = 11
+$Env:BUILD_SOURCEBRANCHNAME    = "develop"
+$Env:BUILD_PROJECTNAME         = "Desenvolvimento"
+$Env:SYSTEM_TASKDEFINITIONSURI = "https://dev.azure.com/praxio/"
+$Env:SYSTEM_ACCESSTOKEN        = [Environment]::GetEnvironmentVariable("XXXXXXXX", "User")
 
 C:\Praxio\GlobusWeb\GlobusWeb.Setup\Scripts\VerificaVersoesInstaladas.ps1
