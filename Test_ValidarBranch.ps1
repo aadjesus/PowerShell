0129b76ad3e75ad4e5cb5f2710ed1f3b8b720a44
aadjesus <aadjesus@outlook.com>
2026-04-23 08:34:03 -0300

    Atualizando arquivos

--- Test_ValidarBranch.ps1
+++ Test_ValidarBranch.ps1
@@ -4,6 +4,7 @@ $Env:RELEASE_ENVIRONMENTNAME    = "QA"
 $Env:BUILD_SOURCEBRANCHNAME     = "Develop"
 $Env:RELEASE_RELEASEDESCRIPTION = ""
 $Env:SYSTEM_ACCESSTOKEN         = [Environment]::GetEnvironmentVariable("XXXXXXXX", "User")
+$Env:SYSTEM_ACCESSTOKEN
 
 $Env:SYSTEM_TEAMFOUNDATIONSERVERURI = "https://vsrm.dev.azure.com/praxio/"
 $Env:SYSTEM_TEAMPROJECTID           = "f7e1f0f0-eca6-49ed-89c4-b580797f7a63"
