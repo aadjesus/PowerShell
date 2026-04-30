aaf6f28631f6d0b65dfbd2befe3717442495b3ca
aadjesus <aadjesus@outlook.com>
2026-04-30 10:29:11 -0300

    Atualizando arquivos

--- Test_ValidarSistemaLiberado.ps1
+++ Test_ValidarSistemaLiberado.ps1
@@ -12,6 +12,6 @@ if (-not (Test-Path "$destino\Oracle.ManagedDataAccess.dll")) {
 $Env:AGENT_WORKFOLDER = $basePath
 $Env:PATH_DESTINO     = "c:\Servers\GlobusWeb"
 $Env:IGNORAR_CONEXAO_BANCO = 11
-$Env:IGNORAR_VALIDAR_SISTEMA = 1
+$Env:IGNORAR_VALIDAR_SISTEMA = 21
 
 C:\Praxio\GlobusWeb\GlobusWeb.Setup\Scripts\ValidarSistemaLiberado.ps1 -sistema 'manutencao'
