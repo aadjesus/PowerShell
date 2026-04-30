0350774762f82da27c083126eebce68e6177e24b
aadjesus <aadjesus@outlook.com>
2026-04-30 20:14:21 -0300

    Atualizando arquivos

--- Criar_TAG.ps1
+++ Criar_TAG.ps1
@@ -10,7 +10,7 @@ Start-Transcript -Path $arquivoLog -Append
 
 
 $VERSION      = "2.0.0"
-$RELEASE_NAME = "2026.Q2.S1"
+$RELEASE_NAME = "2026.Q2.S2"
 $MESSAGE      = "$($RELEASE_NAME)
 
 Task: #574220
@@ -19,7 +19,7 @@ US: #574219"
 $diretorios = Get-ChildItem -Path . -Directory | 
 	Where-Object { 
 		$_.Name -match 'GlobusWeb.*' -and
-        $_.FullName -notmatch '(UIKit|Tools|Servicos)' } |
+        $_.FullName -notmatch '(UIKit|Tools|Setup)' } |
 	Select-Object FullName 
 
 function Exec-Git {
@@ -34,45 +34,49 @@ function Exec-Git {
 }
 
 foreach ($item in $diretorios) {    
-    Write-Host "=========================================================================================================" -ForegroundColor Red
+    Write-Host "=========================================================================================================" -ForegroundColor Green    
     Write-Host $item.FullName
 
     try {
         cd $item.FullName
+        
+        #Write-Host "`nBranch: maste" -ForegroundColor Yellow
+        #Exec-Git "git checkout master"
+        #Exec-Git "git pull origin master"
+        #
+        #Write-Host "`nBranch: develop" -ForegroundColor Yellow
+        #Exec-Git "git checkout develop"
+        #Exec-Git "git pull origin develop"
+        #
+        #Write-Host "`nCriando Branch Release: $RELEASE_NAME" -ForegroundColor Yellow
+        #Exec-Git "git flow release start $RELEASE_NAME"
+        ##
+        #Write-Host "`nCriando TAG: $VERSION" -ForegroundColor Yellow
+        #Exec-Git "git tag -a $VERSION -m '$MESSAGE'"        
+        ##
+        #Write-Host "`nFinalizando Release" -ForegroundColor Yellow
+        #$env:GIT_MERGE_AUTOEDIT = "no"
+        #Exec-Git "git flow release finish -n $RELEASE_NAME"
+        #$env:GIT_MERGE_AUTOEDIT = $null
 
-        Write-Host "Sincronizando branches locais..." -ForegroundColor Yellow
+        #
+        # Enviar
+        #
+		
+        Write-Host "`nEnviando: maste" -ForegroundColor Yellow
         Exec-Git "git checkout master"
-        Exec-Git "git pull origin master"
-        
+        Exec-Git "git push origin master"
+		
+        Write-Host "`nEnviando: develop" -ForegroundColor Yellow
         Exec-Git "git checkout develop"
-        Exec-Git "git pull origin develop"
-        
-        # Write-Host "Iniciando Git Flow Release: $RELEASE_NAME" -ForegroundColor Yellow
-        # Exec-Git "git flow release start $RELEASE_NAME"
-        # 
-        # Write-Host "Tagueando versão $VERSION" -ForegroundColor Yellow
-        # Exec-Git "git tag -a $VERSION -m '$MESSAGE'"
-        # 
-        # 
-        # Write-Host "Finalizando merges locais..." -ForegroundColor Yellow
-        # $env:GIT_MERGE_AUTOEDIT = "no"
-        # Exec-Git "git flow release finish -n $RELEASE_NAME"
-        # $env:GIT_MERGE_AUTOEDIT = $null
-		# 
-        # Write-Host "Enviando alterações para o Azure DevOps..." -ForegroundColor Green
-        # Exec-Git "git checkout master"
-        # Exec-Git "git push origin master"
-		# 
-        # Exec-Git "git checkout develop"
-        # Exec-Git "git push origin develop"
-		# 
-        # Exec-Git "git push origin --tags"
+        Exec-Git "git push origin develop"
+		
+        Write-Host "`nEnviando: tags" -ForegroundColor Yellow
+        Exec-Git "git push origin --tags"
         
     } catch {
-        Write-Host "Erro: $item`n$_" -ForegroundColor Red
-        
+        Write-Host "Erro: $item`n$_" -ForegroundColor Red        
     }
-
 }
 
 cd $Env:PATH_DESTINO
