08ad0e6bfe03b90caacebd394d991884d4327164
aadjesus <aadjesus@outlook.com>
2026-05-12 17:28:57 -0300

    Atualizando arquivos

--- Test_VerificaVersoesInstaladas.ps1
+++ Test_VerificaVersoesInstaladas.ps1
@@ -1,7 +1,7 @@
 ﻿cls
 
 $Env:AGENT_WORKFOLDER          = "c:\azagent\A1\_work\"
-$Env:BUILD_SOURCEBRANCHNAME    = "develop"
+$Env:BUILD_SOURCEBRANCHNAME    = "master"
 $Env:BUILD_PROJECTNAME         = "Desenvolvimento"
 $Env:SYSTEM_TASKDEFINITIONSURI = "https://dev.azure.com/praxio/"
 $Env:SYSTEM_ACCESSTOKEN        = [Environment]::GetEnvironmentVariable("XXXXXXXX", "User")
