7ef15c996d902f0f5163d3dba9185c08355d7cb0
aadjesus <aadjesus@outlook.com>
2026-05-13 10:21:39 -0300

    Atualizando arquivos

--- Test_VerificaVersoesInstaladas.ps1
+++ Test_VerificaVersoesInstaladas.ps1
@@ -1,11 +1,9 @@
 ﻿cls
 
 $Env:AGENT_WORKFOLDER          = "c:\azagent\A1\_work\"
-$Env:BUILD_SOURCEBRANCHNAME    = "master"
+$Env:BUILD_SOURCEBRANCHNAME    = "develop"
 $Env:BUILD_PROJECTNAME         = "Desenvolvimento"
 $Env:SYSTEM_TASKDEFINITIONSURI = "https://dev.azure.com/praxio/"
 $Env:SYSTEM_ACCESSTOKEN        = [Environment]::GetEnvironmentVariable("XXXXXXXX", "User")
 
 C:\Praxio\GlobusWeb\GlobusWeb.Setup\Scripts\VerificaVersoesInstaladas.ps1
-
-
