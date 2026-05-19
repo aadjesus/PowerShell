519d94d69fe0c55863565656feaaa8b779af1172
aadjesus <aadjesus@outlook.com>
2026-05-19 16:13:20 -0300

    Atualizando arquivos

--- Test_FinalizarVersaoInstalada.ps1
+++ Test_FinalizarVersaoInstalada.ps1
@@ -1,8 +1,11 @@
 ﻿cls
+$Env:RELEASE_ARTIFACTS_GlobusWeb_Acidentes_Front_End_DEFINITIONNAME = "GlobusWeb.Acidentes.Front.End"
+$Env:RELEASE_ARTIFACTS_GlobusWeb_Acidentes_Front_End_BUILDNUMBER    = "sssssssss1"
+
 
 $Env:AGENT_WORKFOLDER = 'c:\azagent\A1\_work'
 $Env:NOME_ARTEFATO    = "GlobusWeb.Acidentes.Front.End"
-$Env:VERSAO_NOVA      = "20250815.3"
+$Env:VERSAO_NOVA      = "20260504.1"
 
-C:\Praxio\GlobusWeb\GlobusWeb.Setup\Scripts\FinalizarVersaoInstalada.ps1
+C:\Praxio\GlobusWeb\GlobusWeb.Setup\Scripts\FinalizarVersaoInstalada.ps1 -nomeArtefato "GlobusWeb.Acidentes.Front.End"
 
