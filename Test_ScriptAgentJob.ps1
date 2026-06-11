3e258fddd71c14bbf1188cef4a0196b7d4fec3d8
aadjesus <aadjesus@outlook.com>
2026-06-11 17:56:10 -0300

    Atualizando arquivos

--- Test_ScriptAgentJob.ps1
+++ Test_ScriptAgentJob.ps1
@@ -2,16 +2,17 @@
 
 $Env:BUILD_SOURCEBRANCHNAME         = "588597"
 $Env:SYSTEM_TASKDEFINITIONSURI      = "https://dev.azure.com/praxio/"
-$Env:SYSTEM_TEAMPROJECTID           = "f7e1f0f0-eca6-49ed-89c4-b580797f7a63"
+$Env:SYSTEM_TEAMPROJECT             = "f7e1f0f0-eca6-49ed-89c4-b580797f7a63"
+
 $Env:SYSTEM_TEAMFOUNDATIONSERVERURI = "https://vsrm.dev.azure.com/praxio/"
 
 
 $Env:SYSTEM_ACCESSTOKEN             = [Environment]::GetEnvironmentVariable("XXXXXXXX", "User")
 
 $Env:RELEASE_ENVIRONMENTNAME        = "QA"
-$Env:RELEASE_RELEASEID              = "88573"
-$Env:RELEASE_ENVIRONMENTID          = "126672"
+$Env:RELEASE_RELEASEID              = "88654"
+$Env:RELEASE_ENVIRONMENTID          = "126868"
 $Env:IGNORAR_VALIDAR_BRANCH = $true1
 $Env:ID_DEPLOYMENT_GROUP      = 540
 
-C:\Praxio\GlobusWeb\GlobusWeb.Setup\Scripts\ScriptAgentJob.ps1
+c:\Users\alessandro.augusto\Praxio\GlobusWeb.Setup\Scripts\ScriptAgentJob.ps1
