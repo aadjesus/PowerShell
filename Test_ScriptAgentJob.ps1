b51d99c6bdca3120c478942b3f6324a1debd567b
aadjesus <aadjesus@outlook.com>
2026-06-15 17:58:15 -0300

    Atualizando arquivos

--- Test_ScriptAgentJob.ps1
+++ Test_ScriptAgentJob.ps1
@@ -3,6 +3,7 @@
 $Env:BUILD_SOURCEBRANCHNAME         = "develop"
 $Env:SYSTEM_TASKDEFINITIONSURI      = "https://dev.azure.com/praxio/"
 $Env:SYSTEM_TEAMPROJECT             = "f7e1f0f0-eca6-49ed-89c4-b580797f7a63"
+$Env:BUILD_BUILDNUMBER              = "xxxxxx/$($Env:BUILD_SOURCEBRANCHNAME)"
 
 $Env:SYSTEM_TEAMFOUNDATIONSERVERURI = "https://vsrm.dev.azure.com/praxio/"
 
