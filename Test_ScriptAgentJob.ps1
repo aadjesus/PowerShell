a84b8d11872553eb9f86bf29ea03ce44314551ee
aadjesus <aadjesus@outlook.com>
2026-07-03 12:34:51 -0300

    Atualizando arquivos

--- Test_ScriptAgentJob.ps1
+++ Test_ScriptAgentJob.ps1
@@ -1,7 +1,7 @@
 ﻿cls
 
 $Env:BUILD_SOURCEBRANCHNAME         = "master"
-$Env:BUILD_SOURCEBRANCHNAME         = "develop"
+$Env:BUILD_SOURCEBRANCHNAME1         = "develop"
 $Env:SYSTEM_TASKDEFINITIONSURI      = "https://dev.azure.com/praxio/"
 $Env:SYSTEM_TEAMPROJECT             = "f7e1f0f0-eca6-49ed-89c4-b580797f7a63"
 $Env:BUILD_BUILDNUMBER              = "xxxxxx-$($Env:BUILD_SOURCEBRANCHNAME)"
@@ -12,12 +12,15 @@ $Env:SYSTEM_TEAMFOUNDATIONSERVERURI = "https://vsrm.dev.azure.com/praxio/"
 #$Env:SYSTEM_ACCESSTOKEN             = [Environment]::GetEnvironmentVariable("XXXXXXXX", "User")
 $Env:GLOBUSWEB_PAT                  = [Environment]::GetEnvironmentVariable("XXXXXXXX", "User")
 $Env:SYSTEM_TEAMPROJECTID           = "Desenvolvimento"
-$Env:RELEASE_ENVIRONMENTNAME        = "QA"
+$Env:RELEASE_ENVIRONMENTNAME        = "Dir"
 $Env:RELEASE_RELEASEID              = "88856"
 $Env:RELEASE_ENVIRONMENTID          = "127455"
 
 $Env:RELEASE_RELEASEID              = "89378"
 $Env:RELEASE_ENVIRONMENTID          = "129168"
+
+$Env:RELEASE_RELEASEID                = 89554 
+$Env:RELEASE_ENVIRONMENTID            = 129686
 
 
 
