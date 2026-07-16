e0ea667ceaf34e4d9cd1e4b2c75bd10c2fa13730
aadjesus <aadjesus@outlook.com>
2026-07-16 18:11:46 -0300

    Atualizando arquivos

--- Test_pre-release.ps1
+++ Test_pre-release.ps1
@@ -4,7 +4,7 @@ $Env:BUILD_SOURCEBRANCHNAME         = "master"
 $Env:BUILD_SOURCEBRANCHNAME         = "develop"
 $Env:SYSTEM_TASKDEFINITIONSURI      = "https://dev.azure.com/praxio/"
 $Env:SYSTEM_TEAMPROJECT             = "f7e1f0f0-eca6-49ed-89c4-b580797f7a63"
-$Env:BUILD_BUILDNUMBER              = "aaaaaa-20260621-$($Env:BUILD_SOURCEBRANCHNAME)"
+$Env:BUILD_BUILDNUMBER              = "aaaaaa-20260716-$($Env:BUILD_SOURCEBRANCHNAME)"
 $Env:AGENT_WORKFOLDER               = "c:\azagent\A1\_work\_praxio"
 
 $Env:SYSTEM_TEAMFOUNDATIONSERVERURI = "https://vsrm.dev.azure.com/praxio/"
@@ -30,14 +30,14 @@ $Env:RELEASE_ENVIRONMENTID            = 131273
 $Env:IGNORAR_VALIDAR_BRANCH   = $true1
 $Env:ID_DEPLOYMENT_GROUP      = 817
 
-$Env:SYSTEM_PIPELINESTARTTIME = '2026-07-21 11:48:40-03:00'
+$Env:SYSTEM_PIPELINESTARTTIME = '2026-07-16 11:48:40-03:00'
 
 
 & 'c:\Users\alessandro.augusto\Praxio\Praxio.Tools.Deploy\Modules\pre-release\ValidarBranch.ps1'
 
-& 'c:\Users\alessandro.augusto\Praxio\Praxio.Tools.Deploy\Modules\pre-release\ObterMachineName.ps1'
+#& 'c:\Users\alessandro.augusto\Praxio\Praxio.Tools.Deploy\Modules\pre-release\ObterMachineName.ps1'
 
-#$Env:RUNTIME_JSONPIPELINES = $null
-#& 'c:\Users\alessandro.augusto\Praxio\Praxio.Tools.Deploy\Modules\pre-release\ObterPipelines.ps1' -tags "GlobusWeb,$($Env:RUNTIME_BRANCHORIGIN)"
+$Env:RUNTIME_JSONPIPELINES = $null
+& 'c:\Users\alessandro.augusto\Praxio\Praxio.Tools.Deploy\Modules\pre-release\ObterPipelines.ps1' -tags "GlobusWeb,develop" #$($Env:RUNTIME_BRANCHORIGIN)
 
 #& 'c:\Users\alessandro.augusto\Praxio\Praxio.Tools.Deploy\Modules\pre-release\VerificaVersoesInstaladas.ps1'
