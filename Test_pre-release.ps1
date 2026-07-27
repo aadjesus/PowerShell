afeaf5590a6287d660baa977664b296a58049cbf
aadjesus <aadjesus@outlook.com>
2026-07-27 13:34:26 -0300

    Atualizando arquivos

--- Test_pre-release.ps1
+++ Test_pre-release.ps1
@@ -4,7 +4,7 @@ $Env:BUILD_SOURCEBRANCHNAME         = "master"
 $Env:BUILD_SOURCEBRANCHNAME         = "develop"
 $Env:SYSTEM_TASKDEFINITIONSURI      = "https://dev.azure.com/praxio/"
 $Env:SYSTEM_TEAMPROJECT             = "f7e1f0f0-eca6-49ed-89c4-b580797f7a63"
-$Env:BUILD_BUILDNUMBER              = "aaaaaa-20260725-$($Env:BUILD_SOURCEBRANCHNAME)"
+$Env:BUILD_BUILDNUMBER              = "aaaaaa-20260727-$($Env:BUILD_SOURCEBRANCHNAME)"
 $Env:AGENT_WORKFOLDER               = "c:\azagent\A1\_work\_praxio"
 
 $Env:SYSTEM_TEAMFOUNDATIONSERVERURI = "https://vsrm.dev.azure.com/praxio/"
