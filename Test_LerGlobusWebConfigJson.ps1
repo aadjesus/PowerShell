823faedc42ed3137f03e18beedf3268b7615a629
aadjesus <aadjesus@outlook.com>
2026-09-09 07:09:15 -0300

    Atualizando arquivos

--- Test_LerGlobusWebConfigJson.ps1
+++ Test_LerGlobusWebConfigJson.ps1
@@ -1,3 +1,5 @@
 ﻿cls
-$env:PATH_BASE = "c:\Servers"
+$env:PATH_BASE    = "c:\Servers"
+$env:NOME_PRODUTO = "GlobusWeb"
+#$env:NOME_PRODUTO = $null
 & "C:\Users\alessandro.augusto\Praxio\GlobusWeb\GlobusWeb.Setup\Scripts\LerGlobusWebConfigJson.ps1"
